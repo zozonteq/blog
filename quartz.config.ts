eb78f584daa2aa06c6adde3487b94f7ba644daac
oto <ottotak@outlook.com>
2024-11-18 10:06:50 +0900

    Quartz sync: Nov 18, 2024, 10:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "memo.zozsys.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
